09c19ac2b2051f8e89b7089c628298ea32d93bf7
lanchi <lanchiagrawal11@gmail.com>
2021-08-17 18:38:48 +0530

    change server.js file

--- server.js
+++ server.js
@@ -1,17 +1,10 @@
 const jsonServer = require("json-server");
 const server = jsonServer.create();
-const router = jsonServer.router("./db.json"); // <== Will be created later
-const middlewares = jsonServer.defaults({
-  static: "./build",
-});
-const port = process.env.PORT || 8000; // <== You can change the port
+const router = jsonServer.router("db.json"); // <== Will be created later
+const middlewares = jsonServer.defaults();
+const port = process.env.PORT || 3200; // <== You can change the port
 
 server.use(middlewares);
-server.use(
-  jsonServer.rewriter({
-    "/api/*": "/$1",
-  })
-);
 server.use(router);
 
 server.listen(port);
